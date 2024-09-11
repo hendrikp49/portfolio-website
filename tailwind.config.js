62c39075abc16ac9f2d41500fbbccbd81424f60a
Hendrik <hendrikp49@gmail.com>
2024-09-11 22:34:00 +0800

    add simple nvabar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        sm: "480px",
+      },
+
       fontFamily: {
         heading: ["Playfair Display, serif"],
         content: ["Raleway, sans-serif"],
